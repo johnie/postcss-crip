f327621bbf8f18ec1c35763e2cdca7d61d9e1b03
Johnie Hjelm <johnie.hjelm@symbio.com>
2015-07-27 17:16:52 +0200

    Added options

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 var postcss = require('postcss');
 
-module.exports = postcss.plugin('postcss-crip', function () {
+/**
+ * Merge two objects into one object.
+ *
+ * @param {object} obj1
+ * @param {object} obj2
+ *
+ * @return {object}
+ */
 
-    var PROPS = {
+function merge(obj1, obj2) {
+    for (var key in obj1) {
+        if (obj2[key] === undefined) {
+            obj2[key] = obj1[key];
+        } else if (obj2[key] instanceof Array) {
+            for (var child in obj1[key]) {
+                if (obj2[key].indexOf(obj1[key][child]) !== -1) {
+                    obj2[key].push(obj1[key][child]);
+                }
+            }
+        } else if (typeof obj2[key] === 'string') {
+            obj2[key] = [obj2[key]];
+            for (var kid in obj1[key]) {
+                if (obj2[key].indexOf(obj1[key][kid]) !== -1) {
+                    obj2[key].push(obj1[key][kid]);
+                }
+            }
+        }
+    }
+
+    return obj2;
+}
+
+module.exports = postcss.plugin('postcss-crip', function (options) {
+
+    options = options || {};
+
+    var DEFAULTS = {
       'bg':  ['background'],
       'bgc': ['background-color'],
       'bgz': ['background-size'],
@@ -47,6 +81,8 @@ module.exports = postcss.plugin('postcss-crip', function () {
       'ws':  ['white-space'],
       'zi':  ['z-index']
     };
+
+    var PROPS = merge(DEFAULTS, options);
 
     return function (css) {
 
